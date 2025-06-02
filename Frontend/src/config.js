79bba1475f3e25cb53e3598e7d6414423ba4345a
Soham Patil <sohampatilsp55@gmail.com>
2025-06-02 22:35:21 +0530

    frotnend config.js modified

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/api/v1'
+export const NODE_API_URL = 'https://mediease-node-api.onrender.com/api/v1'
+export const BASE_URL = NODE_API_URL;
+export const FLASK_PRESCRIPTION_API_URL = 'https://mediease-chatbot-api.onrender.com'
 export const token = localStorage.getItem("token")
