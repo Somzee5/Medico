f7f6f0d6151202d6211770f2e9b82d05db08aeab
Soham Patil <sohampatilsp55@gmail.com>
2025-06-03 12:22:15 +0530

    Dimensions changed

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -115,7 +115,7 @@ const Header = () => {
       <div className="container">
         <div className="flex items-center justify-between">
           <div>
-            <img src={logo} alt="Logo" className="h-[40px] w-[40px]" />
+            <img src={logo} alt="Logo" className="h-[50px] w-auto" />
           </div>
 
           <div className="navigation one" ref={menuRef} onClick={toggleMenu}>
