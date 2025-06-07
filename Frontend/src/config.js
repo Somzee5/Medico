6fd3c6f52794732c99218ccebffa41aa0a75df9f
Soham Patil <sohampatilsp55@gmail.com>
2025-06-07 19:38:37 +0530

    Default profile picture is updated

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -3,4 +3,7 @@ export const FLASK_PRESCRIPTION_API_URL = 'https://mediease-chatbot-api.onrender
 export const ML_CHATBOT_API_URL = 'https://chatbot-y6gz.onrender.com'
 export const BASE_URL = NODE_API_URL;
 export const token = localStorage.getItem("token")
-export const DEFAULT_PROFILE_PICTURE = "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg";
+// export const DEFAULT_PROFILE_PICTURE = "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg";
+
+
+export const DEFAULT_PROFILE_PICTURE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNyRUxhwOHmKFRfJMPOQbT1a2xF1IBprnslNx0sbipxugwsJHuhEtE8Azz9W9KdmWSzr0&usqp=CAU";
