7ae46731e47dfa28a1df1ef487d25d0619b78ec5
Soham Patil <sohampatilsp55@gmail.com>
2025-06-07 21:34:03 +0530

    changes at ambulance backend

--- Backend/Routes/ambulance.js
+++ Backend/Routes/ambulance.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getSingleAmbulance, getAllAmbulance, deleteAmbulance, getAmbulanceProfile, updateAvailabilityAndLocation,getNearbyAmbulances, updateAmbulance } from '../Controllers/ambulanceController.js'
+import { getSingleAmbulance, getAllAmbulance, deleteAmbulance, getAmbulanceProfile, updateAvailabilityAndLocation, getNearbyAmbulances } from '../Controllers/ambulanceController.js'
 import { authenticate, restrict } from '../auth/verifyToken.js'
 import { createAmbulanceBooking, AmbulancenewBooking } from '../Controllers/ambulanceBookingController.js'
 
@@ -7,12 +7,11 @@ const router = express.Router()
 
 router.get('/:id', getSingleAmbulance)
 router.get('/', getAllAmbulance)
-router.put('/:id', authenticate, restrict(['ambulance_service']), updateAmbulance)
 router.delete('/:id', authenticate, restrict(['ambulance_service']), deleteAmbulance)
 router.get('/profile/me', authenticate, restrict(['ambulance_service']), getAmbulanceProfile)
 router.delete('/profile/me', authenticate, restrict(['ambulance_service']), deleteAmbulance)
-router.post('/bookings/checkout-session-razorpay/:ambulanceId',authenticate,createAmbulanceBooking)
-router.post('/bookings/newbooking',authenticate,AmbulancenewBooking)
+router.post('/bookings/checkout-session-razorpay/:ambulanceId', authenticate, createAmbulanceBooking)
+router.post('/bookings/newbooking', authenticate, AmbulancenewBooking)
 router.patch('/availability', authenticate, restrict(['ambulance_service']), updateAvailabilityAndLocation)
 router.post('/nearby',  getNearbyAmbulances)
 export default router
